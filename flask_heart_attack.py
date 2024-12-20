caa2703cf85d0c9e556f13eed7106b726b32aed8
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2024-12-20 22:51:53 +0800

    Create server for heart attack prediction

---
+++ flask_heart_attack.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import os
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+@app.route('/heart-attack-prediction', methods=['POST'])
+def get_prediction():
+    sex = request.json['sex']
+    age = request.json['age']
+    chest_pain = request.json['chest_pain']
+    smoking = request.json['smoking']
+    abnormality = request.json['abnormality']
+
+    print(sex, age, chest_pain, smoking, abnormality,)
+
+    prediction = make_heart_attack_prediction(sex, age, chest_pain, smoking, abnormality)
+    prediction = {'prediction': prediction}
+    return jsonify(prediction)
+
+
+
+def make_heart_attack_prediction(sex, age, chest_pain, smoking, abnormality):
+    prediction = True
+    # insert ml model here
+    return prediction
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
